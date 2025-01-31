1bc0b83dad0a05016b2967b2c888ad96627de636
Paiton Bertschy <paitonbertschy@gmail.com>
2025-01-31 16:36:50 -0600

    create a test command to make sure this works correctly

---
+++ commands/faq/faq.js
@@ -0,0 +1,28 @@
+/**
+ * @file test faq command
+ * @author Naman Vrati
+ * @since 1.0.0
+ * @version 3.3.0
+ */
+
+// Deconstructing prefix from config file to use in help command
+const { prefix } = require("./../../config.json");
+
+// Deconstructing EmbedBuilder to create embeds within this command
+const { EmbedBuilder, ChannelType } = require("discord.js");
+
+/**
+ * @type {import('../../typings').LegacyCommand}
+ */
+module.exports = {
+    name: "hello",
+    description: "Sends an embed that says Hello World!",
+    execute(message, args) {
+        const helloEmbed = new EmbedBuilder()
+            .setColor(0x0099ff)
+            .setTitle("Hello World!")
+            .setDescription("This is an embed that says Hello World!");
+
+        message.channel.send({ embeds: [helloEmbed] });
+    },
+};
