37f243de4cb004bdbb59e9d2b6dc6738f4bcd202
Paiton Bertschy <paitonbertschy@gmail.com>
2025-02-01 10:23:24 -0600

    Revert "Remove embed command files for 'chuni', 'chusan', 'maimai', and 'wacca'"
    This reverts commit c27cbcb52e5e6ea537c49367a564047134261044.

---
+++ commands/chuni.js
@@ -0,0 +1,17 @@
+const { EmbedBuilder } = require('discord.js');
+
+module.exports = {
+    name: 'chuni',
+    description: 'Sends an embed with a header, subheader, and body text',
+    execute(message, args) {
+        const embed = new EmbedBuilder()
+            .setColor(0x0099ff)
+            .setTitle('Header')
+            .setDescription('Subheader')
+            .addFields(
+                { name: 'Body', value: 'This is the body text of the embed.' }
+            );
+
+        message.channel.send({ embeds: [embed] });
+    },
+};
